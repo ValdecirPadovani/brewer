00c7421c7dd5e68d08789eae8adab4d282936f16
valdecir.junior <valdecirpadovani.jr@gmail.com>
2018-07-17 13:36:17 -0300

    Final Aula 3

--- src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -1,9 +1,17 @@
 package com.algaworks.brewer.model;
 
+import org.hibernate.validator.constraints.NotBlank;
+
 public class Cerveja {
 	
+	@NotBlank
 	private String sku;
+	
+	@NotBlank
 	private String nome;
+	
+	
+	private String descricao;
 	
 	public String getSku() {
 		return sku;
@@ -16,5 +24,11 @@ public class Cerveja {
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
 }
